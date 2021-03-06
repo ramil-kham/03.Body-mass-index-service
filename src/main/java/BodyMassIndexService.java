4192b017e049ebf13b73e33da431aed1d3606577
ramil-kham <ramil66-2010@mail.ru>
2021-03-06 23:11:18 +0300

    fixed #1

--- src/main/java/BodyMassIndexService.java
+++ src/main/java/BodyMassIndexService.java
@@ -1,6 +1,6 @@
 public class BodyMassIndexService {
     double calculate (double weight, double heightSm) {
-        double index = weight / (heightSm * heightSm / 10_000); // делим на 10_000, чтобы перейти из см2 в м2
+        double index = weight / (heightSm * heightSm / 10_000); // делим на 10000, чтобы перейти из см2 в м2
         return index;
     }
 }
